7d4f00512e81e88fd9dda542983819a4b5375ec0
talk2atef <159116533+talk2atef@users.noreply.github.com>
2024-02-06 13:26:26 +0530

    Update jenkins.sh
    remove dot when execute

--- jenkins.sh
+++ jenkins.sh
@@ -12,4 +12,4 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y 
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins -y.
